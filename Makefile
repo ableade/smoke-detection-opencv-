06aa7205509c6923f020f33d68b0dbf902afe6e1
Adebodun Adekunle <sola@kosherkampus.com>
2018-08-07 10:59:44 -0400

    simple neural net

---
+++ Makefile
@@ -0,0 +1,34 @@
+SOURCES = $(wildcard *.cpp)
+OBJECTS = $(SOURCES:.cpp=.o)
+
+# Use the name of the current directory as the name of the executable: 
+all:  simpleneuralnet
+
+
+DEPDIR = deps
+$(shell mkdir -p $(DEPDIR) >/dev/null)
+
+CXXFLAGS += -std=c++11
+CXXFLAGS += -O3
+CXXFLAGS += -g
+CXXFLAGS += -Wall -Wextra -pedantic
+CXXFLAGS += -Weffc++
+CXXFLAGS += -Werror=reorder
+CXXFLAGS += -Werror=return-type
+
+OPENCV_LINK_FLAGS += -lopencv_core
+OPENCV_LINK_FLAGS += -lopencv_highgui
+OPENCV_LINK_FLAGS += -lopencv_imgcodecs
+OPENCV_LINK_FLAGS += -lopencv_imgproc
+OPENCV_LINK_FLAGS += -lopencv_videoio
+OPENCV_LINK_FLAGS += -lopencv_ml
+
+LFLAGS += -lboost_filesystem
+LFLAGS += -lboost_system
+LFLAGS += $(OPENCV_LINK_FLAGS)
+
+simpleneuralnet : simpleneuralnetwork.cpp
+	$(CXX) -o $@ $< $(LFLAGS)
+
+clean:
+	rm -f $(OBJECTS) $(addprefix $(DEPDIR)/, $(DEPS)) simpleneuralnet
--- simpleneuralnetwork.cpp
+++ simpleneuralnetwork.cpp
@@ -1,31 +1,39 @@
 #include <boost/filesystem.hpp>
 #include <iterator>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace boost::filesystem;
-
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cerr;
+using std::cin;
 
 /*
-Adds training data images from directory.
-Returns number of images added
+Adds training data images from directory. Images in training directory are expected to be JPG 
+300 * 300. Returns number of images added
 */
-int addTrainingData(string directory, cv::Mat m) {
+int addTrainingData(string directory, cv::Mat& m) {
     auto count =0;
-    if(is_directory(p))
+    if(is_directory(directory))
     {
-        copy(directory_iterator(p), directory_iterator(), back_inserter(v));
-        std::cout << p << " is a directory containing:\n";
-        cv::Vec_2i positve {1,0};
-        cv::Vec_2i negative {0,1}; 
-        for ( std::vector<directory_entry>::const_iterator it = v.begin(); it != v.end();  ++ it )
+        std::vector<directory_entry> v;
+        copy(directory_iterator(directory), directory_iterator(), back_inserter(v)); 
+        for (auto it = v.begin(); it != v.end();  ++ it )
         {
-            cv::Mat img = cv::imread((*it).path);
-            cv::Mat cnv;
-            img.convertTo(cnv, CV_32FC1);
-            cv::Mat m_flat = img.reshape(1,1); // unroll image into single channel vector
-            m.push_back(m_flag);
-            count++;
+            cv::Mat img = cv::imread(it->path().string());
+            if(img.rows == 300 && img.cols == 300) {
+                cv::Mat cnv;
+                img.convertTo(cnv, CV_32FC1);
+                cv::Mat m_flat = cnv.reshape(1,1); // unroll image into single channel vector
+                m.push_back(m_flat);
+                count++;
+                //cout << "Processed image "<< it->path().string() <<endl;
+            }
         } 
     }
     return count;
@@ -37,13 +45,64 @@ int main(int argc, char *argv[])
         cout << "Program usage : <training directory>   <test directory>"<<endl;
         exit(1);
     }
-    path positivePath(argc>1? argv : "."), negativePath(argc>1?argv[2]:".");
-    cv::Mat trainingData;
-    auto pCount = addTrainingData(positivePath);
-    auto nCount = addTrainingData(negativePath);
-    vector<float> responses((pCount + nCount), 1);
 
+    path positivePath(argc>1? argv[1] : "."), negativePath(argc>1?argv[2]:".");
+    cv::Mat trainingData;
+    auto pCount = addTrainingData(positivePath.string(), trainingData);
+    auto nCount = addTrainingData(negativePath.string(), trainingData);
+
+    vector<float> responses((pCount + nCount), 1);
     for (int i = pCount -1; i < responses.size(); ++i) {
         responses[i] = 0;
     }
+    
+    cout <<"Size of training daa  is "<<responses.size()<<endl;
+
+    auto trainDataSet = cv::ml::TrainData::create(trainingData, cv::ml::ROW_SAMPLE, cv::Mat(responses, true), 
+        cv::noArray(), cv::noArray(), cv::noArray(), cv::noArray());
+
+    //We will only use 80% of our data set for training.
+    trainDataSet->setTrainTestSplitRatio(0.8);
+
+    
+	cv::Ptr<cv::ml::ANN_MLP> nn = cv::ml::ANN_MLP::create();
+    cout << "Created neural network"<<endl;
+
+	//Neural network with three layers and 512 nodes
+	std::vector<int> layerSizes = { 270000, 512, 1 };
+	nn->setLayerSizes(layerSizes);
+    cout << "Set neural network with 3 layers"<<endl;
+
+	nn->train(trainDataSet);
+
+    cout << "Calcuating error for neural network"<<endl;
+
+    cv::Mat testResponses;
+    auto error = nn->calcError(trainDataSet, true, testResponses);
+    cout << "Percentage error was "<<error<<" percent"<<endl;
+    cout << "Size of test set was "<<testResponses.size()<<endl;
+
+    string answer;
+
+    while (1) {
+        cout << "Enter the path to an image to detect if it contains smoke or enter quit to exit"<<endl;
+        cin >> answer;
+        if (answer == "quit") {
+            break;
+        }
+        path inputImage(answer);
+        cv::Mat img = cv::imread(inputImage.string());
+        if (img.empty())
+        {
+            cerr << "WARNING: Could not read image." << std::endl;
+            continue;
+        } else {
+            cv::Mat cnv;
+            img.convertTo(cnv, CV_32FC1);
+            cv::Mat m_flat = cnv.reshape(1,1); // unroll image into single channel vector
+            auto prediction = nn->predict(m_flat);
+            cout << "prediciton was "<< prediction<<endl;
+        }
+    }
+	return (0);
 }
